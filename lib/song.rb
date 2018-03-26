693e18fe836c5fb6b7b21a59fe6abfaaa32c6d88
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-26 04:10:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,10 +5,18 @@ class Song
   def self.create
     x = Song.new
     @@all << x
+    x
   end
 
   def self.all
     @@all
+  end
+
+  def self.new_by_name(name)
+    x = Song.new
+    x.name = name
+    @all << x
+    x
   end
 
   def save
